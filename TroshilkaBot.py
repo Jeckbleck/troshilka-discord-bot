87d46ead9f4bacac260f62b8a110cd3258eb4f1a
Radislav <r.stefanov@student.fontys.nl>
2024-10-26 17:18:37 +0200

    fix

---
+++ TroshilkaBot.py
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+=======
+import discord
+from discord.ext import commands
+import asyncio
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.command(name='sound_1')
+async def play_sound(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        voice_client = await channel.connect()
+
+        source = discord.FFmpegPCMAudio("audio path", executable="ffmpeg path")
+        voice_client.play(source, after=lambda e: print("Finished playing sound1"))
+
+        while voice_client.is_playing():
+            await asyncio.sleep(1)
+        await voice_client.disconnect()
+    else:
+        await ctx.send("You need to be in a voice channel to use this command.")
+
+bot.run("token")
+>>>>>>> ab83bd6 (simple discord bot that plays a single audio)
